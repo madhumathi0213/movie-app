dc369a1537c464c50c7a7467dfb821f33d369999
madhumathi_allimuthu <madhumathi@pentafox.in>
2020-10-20 09:20:31 +0530

    commands change

--- src/Components/Hooks/useMovieFetch.js
+++ src/Components/Hooks/useMovieFetch.js
@@ -14,16 +14,11 @@ export const useMovieFetch = movieId => {
       const endpoint = `${API_URL}movie/${movieId}?api_key=${API_KEY}`;
       const result = await (await fetch(endpoint)).json();
 
-      const creditsEndpoint = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
-      const creditsResult = await (await fetch(creditsEndpoint)).json();
-      const directors = creditsResult.crew.filter(
-        member => member.job === 'Director'
-      );
+      
 
       setState({
         ...result,
-        actors: creditsResult.cast,
-        directors,
+       
       })
 
     } catch (error) {
